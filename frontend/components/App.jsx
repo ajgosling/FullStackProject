00b3bb6025ec26f8ec2a3fadc867b537927688c4
ajgosling <ajgosling2@gmail.com>
2018-10-06 17:52:35 -0700

    improving auth functionality

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-
 import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 
 const App = () => (
   <div>
     <header>
-      <h1>Slack</h1>
+      <h1>Quack</h1>
       <GreetingContainer />
     </header>
+
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
-);
+)
 
 export default App;
