8eb011aacb1702723da9fe39e6db955a05c2fca9
ajgosling <ajgosling2@gmail.com>
2018-10-08 18:02:18 -0700

    Implemented homepage css

---
+++ frontend/components/greeting/greeting-body.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+// import ducks from '../../../assets/images/ducks.png';
+
+const GreetingBody = () => (
+  <div className='body'>
+    <div className='image-container'>
+      <img className='new-hotness' src={window.images.ducks} />
+
+    </div>
+    <div className='body-container'>
+      <p className='body-headline'>
+        Fowl Language Encouraged
+      </p>
+      <p className='body-info'>
+        When your team needs to kick off a project,
+        hire a new employee, deploy some code,
+        review a sales contract, finalize next year's budget,
+        measure an A/B test, plan your next office opening,
+        and more, Quack has you covered.
+      </p>
+
+      <Link className="body-signup" to="/signup">GET STARTED</Link>
+      <p className="body-signin" >Already using Slack? <Link className="body-signin-link" to="/login">Sign In</Link></p>
+
+    </div>
+  </div>
+
+);
+
+export default GreetingBody;
---
+++ frontend/components/greeting/greeting-header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+// import logo from '../../../app/assets/images/logo.png';
+// import logo from './slack-logo.png';
+
+const GreetingHeader = () => (
+  <header className='greeting-header'>
+    <ul className='greeting-header-list'>
+      <img className='quack-logo' src={window.images.logo} />
+      <li className='why-quack'><span>Why Quack?</span></li>
+      <li><span>Solutions</span></li>
+      <li><span>Resources</span></li>
+      <li><span>Pricing</span></li>
+      <li className="greeting-header-login">
+        <span><Link to="/login">Sign In</Link></span>
+      </li>
+      <Link className="greeting-header-signup" to="/signup">GET STARTED</Link>
+    </ul>
+  </header>
+
+);
+
+export default GreetingHeader;
