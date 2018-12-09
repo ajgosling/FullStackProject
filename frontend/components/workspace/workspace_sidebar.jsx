0bba9d04c1131d462db919a916c71999d8a99d85
ajgosling <ajgosling2@gmail.com>
2018-12-08 16:30:34 -0800

    add github logos

--- frontend/components/workspace/workspace_sidebar.jsx
+++ frontend/components/workspace/workspace_sidebar.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
 import ChannelList from '../channel/channel_list';
 import ChannelForm from '../channel/channel_form';
 import DirectMessageList from '../channel/direct_message_list';
@@ -20,44 +17,35 @@ class WorkspaceSidebar extends React.Component {
         normalChannels[channel.id] = channel
       }
     })
-    return (
-      <div className="workspace-sidebar">
+    return <div className="workspace-sidebar">
         <div className="current-user-info">
           <div className="workspace-info">
             <h1 className="workspace-name">Rabbit-Season</h1>
             <div className="down-caret">v</div>
-
           </div>
           <div className="user-logout">
-
             <h2>
-              <img className='online-dot' src={window.images.online} />
-              {this.props.currentUser.username}</h2>
+              <img className="online-dot" src={window.images.online} />
+              {this.props.currentUser.username}
+            </h2>
             <button onClick={this.props.logout}>Logout</button>
           </div>
-
         </div>
         <div className="channels-and-messages">
-          <ChannelList
-            channels={normalChannels}
-            selectedChannel={this.props.selectedChannel}
-            />
+          <ChannelList channels={normalChannels} selectedChannel={this.props.selectedChannel} />
 
-          <DirectMessageList
-            channels={directMessages}
-            selectedChannel={this.props.selectedChannel}
-
-            />
+          <DirectMessageList channels={directMessages} selectedChannel={this.props.selectedChannel} />
         </div>
 
         <ChannelForm />
         <DirectMessageForm />
         <div className="website-links">
-          <i className="fab fa-github"></i>
-        </div>
-      </div>
+          <i className="fab fa-github" />
+          <i className="fab fa-github" />
+          <i className="fab fa-github" />
 
-    );
+        </div>
+      </div>;
   }
 }
 
